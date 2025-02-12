1c79c2f046cd05882801b29b1a79a63a171e5331
charlie-san <charliesan@envs.net>
2025-02-12 19:52:19 +1100

    please let this work
    Signed-off-by: charlie-san <charliesan@envs.net>

---
+++ Tests.md
@@ -0,0 +1,10 @@
+# JMASM tests
+
+
+Writing Tests for JMASM is easy, we are using the [Junit5 framework](https://junit.org/junit5/).
+
+## How to write tests ether new or existing
+
+1. Ether create a new test class or add a new test method to an existing test class.
+2. Add the `@Test` annotation to the test method.
+3. Add the `@DisplayName` annotation to the test method to give it a meaningful name.
---
+++ src/test/java/MainTest.java
@@ -0,0 +1,31 @@
+
+
+import org.junit.jupiter.api.*;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class MainTest {
+    @Nested
+    @DisplayName("Main Function Tests")
+    class MainFunctionTests {
+        @BeforeEach
+        void setUp() {
+            // TODO Setup for main function tests
+        }
+
+        @Test
+        @DisplayName("Test Main Initialization")
+        void testMainInitialization() {
+            // TODO Test main initialization
+        }
+    }
+
+    @Nested
+    @DisplayName("Integration Tests")
+    class IntegrationTests {
+        @Test
+        @DisplayName("Test Full Program Flow")
+        void testProgramFlow() {
+            // TODO Test complete program flow
+        }
+    }
+}
