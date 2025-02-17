095614233adb9c91f73f25706cacfe94c2dfff46
charlie-san <charliesan@envs.net>
2025-02-17 22:37:38 +1100

    implemented MSF 1 (Native interface)
    Signed-off-by: charlie-san <charliesan@envs.net>

---
+++ src/main/java/org/Finite/Exceptions/MASMException.java
@@ -0,0 +1,32 @@
+package org.Finite.Exceptions;
+
+public class MASMException extends RuntimeException {
+    private final int lineNumber;
+    private final String instruction;
+    private final String details;
+
+    public MASMException(String message, int lineNumber, String instruction, String details) {
+        super(message);
+        this.lineNumber = lineNumber;
+        this.instruction = instruction;
+        this.details = details;
+    }
+
+    @Override
+    public String getMessage() {
+        return String.format("Line %d: %s\nInstruction: %s\nDetails: %s", 
+            lineNumber, super.getMessage(), instruction, details);
+    }
+
+    public int getLineNumber() {
+        return lineNumber;
+    }
+
+    public String getInstruction() {
+        return instruction;
+    }
+
+    public String getDetails() {
+        return details;
+    }
+}
