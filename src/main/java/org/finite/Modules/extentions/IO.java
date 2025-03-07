1ed350d3d74590e8100e75e0dde52e24ff4ce9b0
charliesan <charliesan@envs.net>
2025-03-08 02:28:16 +1100

    Â

--- src/main/java/org/finite/Modules/extentions/IO.java
+++ src/main/java/org/finite/Modules/extentions/IO.java
@@ -4,6 +4,7 @@ import org.finite.Functions;
 import org.finite.ModuleManager.MNIMethodObject;
 import org.finite.ModuleManager.annotations.MNIClass;
 import org.finite.ModuleManager.annotations.MNIFunction;
+import org.finite.*;
 import org.finite.Parsing;
 @MNIClass("IO")
 public class IO {
